f1b439454d8040ce94b11c71a6c331b1aaa7198d
Casey Milne <cjmofficio@gmail.com>
2026-03-01 13:42:02 -0800

    Set dark mode, remove old templates for showcase.

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> class="dark">
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-<body class="bg-slate-50 text-slate-900 dark:bg-slate-900 dark:text-slate-50">
+<body <?php body_class('bg-black text-white'); ?>>
 <?php wp_body_open(); ?>
 
 <?php require_once get_template_directory() . '/templates/site-header.php'; ?>
